9086307f7aab163bc3ee36e1a63cf7d1ddf9a563
tqcq <github-actioin@github-action.com>
2023-07-30 08:50:57 +0000

    feature auto save to cloud on start game

--- games/adarkroom/script/engine.js
+++ games/adarkroom/script/engine.js
@@ -372,6 +372,10 @@
         }
         need_cloud_save = true;
         localStorage.gameState = JSON.stringify(State);
+        if (localStorage.binId == undefined) {
+          localStorage.binId = "empty";
+          Engine.saveToCloud();
+        }
       }
     },
 
